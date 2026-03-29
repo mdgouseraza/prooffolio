bba8e58042238608aac9154418c0c47e1a081be1
Mohammed Gouse Raza <mdgouseraza.cse@gmail.com>
2026-03-29 15:18:03 +0530

    Update DATABASES settings to use dj_database_url
    Refactor database settings to use dj_database_url for configuration.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,19 +61,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql"
-        if os.getenv("DATABASE_URL")
-        else "django.db.backends.sqlite3",
-        "NAME": os.getenv("POSTGRES_DB", BASE_DIR / "db.sqlite3"),
-        "USER": os.getenv("POSTGRES_USER", ""),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-        "HOST": os.getenv("POSTGRES_HOST", ""),
-        "PORT": os.getenv("POSTGRES_PORT", ""),
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
