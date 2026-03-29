977272ee4afd6576d7b62e37b89b50c805caaec9
Mohammed Gouse Raza <mdgouseraza.cse@gmail.com>
2026-03-29 15:43:03 +0530

    Change DATABASE_URL parsing to use sqlite default

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,10 +64,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True,
     )
 }
 AUTH_PASSWORD_VALIDATORS = [
